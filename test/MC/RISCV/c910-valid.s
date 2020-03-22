cf6168e1df29561f99389a1d4c0ddea63a413729
chenying <1357640266@qq.com>
2020-03-22 13:57:00 +0800

    add computation and sync instructions and testcases

--- test/MC/RISCV/c910-valid.s
+++ test/MC/RISCV/c910-valid.s
@@ -32,3 +32,64 @@ tst a0, a1, 1
 # CHECK-ASM-AND-OBJ: tstnbz a0, a1
 # CHECK-ASM: encoding: [0x0b,0x95,0x05,0x80]
 tstnbz a0, a1
+
+# CHECK-ASM-AND-OBJ: mula a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x20]
+mula a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: addsl a1, a2, a3, 1
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x02]
+addsl a1, a2, a3, 1
+
+# CHECK-ASM-AND-OBJ: mulah a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x28]
+mulah a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: mulaw a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x20]
+mulaw a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: muls a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x22]
+muls a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: mulsh a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x2a]
+mulsh a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: mulsw a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x20]
+mulsw a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: mveqz a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x20]
+mveqz a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: mvnez a1, a2, a3
+# CHECK-ASM: encoding: [0x8b,0x15,0xd6,0x42]
+mvnez a1, a2, a3
+
+# CHECK-ASM-AND-OBJ: srri a0, a1, 4
+# CHECK-ASM: encoding: [0x0b,0x95,0x45,0x10]
+srri a0, a1, 4
+
+# CHECK-ASM-AND-OBJ: srriw a0, a1, 3
+# CHECK-ASM: encoding: [0x0b,0x95,0x35,0x14]
+srriw a0, a1, 3
+
+# CHECK-ASM-AND-OBJ: sync
+# CHECK-ASM: encoding: [0x0b,0x00,0x80,0x01]
+sync
+
+# CHECK-ASM-AND-OBJ: sync_i
+# CHECK-ASM: encoding: [0x0b,0x00,0xa0,0x01]
+sync_i
+
+# CHECK-ASM-AND-OBJ: sync_is
+# CHECK-ASM: encoding: [0x0b,0x00,0xb0,0x01]
+sync_is
+
+# CHECK-ASM-AND-OBJ: sync_s
+# CHECK-ASM: encoding: [0x0b,0x00,0x90,0x01]
+sync_s
+
