d7a341bf398c9f9472831b503a859386c2650085
chenying <1357640266@qq.com>
2020-03-23 10:13:13 +0800

    add c910 load and store extended instructions and testcases

--- test/MC/RISCV/c910-valid.s
+++ test/MC/RISCV/c910-valid.s
@@ -93,3 +93,236 @@ sync_is
 # CHECK-ASM: encoding: [0x0b,0x00,0x90,0x01]
 sync_s
 
+
+# CHECK-ASM-AND-OBJ: flrd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xe5,0xc5,0x62]
+flrd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: flrw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xe5,0xc5,0x42]
+flrw a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: flurd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xe5,0xc5,0x72]
+flurd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: flurw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xe5,0xc5,0x52]
+flurw a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: fsrd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xf5,0xc5,0x62]
+fsrd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: fsrw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xf5,0xc5,0x42]
+fsrw a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: fsurd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xf5,0xc5,0x72]
+fsurd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: fsurw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xf5,0xc5,0x52]
+fsurw a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lrb a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x02]
+lrb a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lrbu a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x82]
+lrbu a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lrd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x62]
+lrd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lrh a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x22]
+lrh a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lrhu a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0xa2]
+lrhu a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lrw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x42]
+lrw a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lrwu a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0xc2]
+lrwu a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lurb a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x12]
+lurb a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lurbu a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x92]
+lurbu a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lurd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x72]
+lurd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lurh a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x32]
+lurh a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lurhu a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0xb2]
+lurhu a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lurw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0x52]
+lurw a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: lurwu a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xc5,0xc5,0xd2]
+lurwu a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: srb a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x02]
+srb a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: srd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x62]
+srd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: srh a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x22]
+srh a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: srw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x42]
+srw a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: surb a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x12]
+surb a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: surd a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x72]
+surd a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: surh a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x32]
+surh a0, a1, a2, 1
+
+# CHECK-ASM-AND-OBJ: surw a0, a1, a2, 1
+# CHECK-ASM: encoding: [0x0b,0xd5,0xc5,0x52]
+surw a0, a1, a2, 1
+
+
+# CHECK-ASM-AND-OBJ: lbia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x18]
+lbia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lbib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x08]
+lbib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lbuia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x98]
+lbuia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lbuib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x88]
+lbuib a0, (a1), 1, 4
+
+
+# CHECK-ASM-AND-OBJ: ldia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x78]
+ldia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: ldib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x68]
+ldib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lhia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x38]
+lhia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lhib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x28]
+lhib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lhuia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0xb8]
+lhuia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lhuib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0xa8]
+lhuib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lwia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x58]
+lwia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lwib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0x48]
+lwib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lwuia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0xd8]
+lwuia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: lwuib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xc5,0x15,0xc8]
+lwuib a0, (a1), 1, 4
+
+
+# CHECK-ASM-AND-OBJ: sbia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x18]
+sbia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: sbib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x08]
+sbib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: sdia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x78]
+sdia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: sdib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x68]
+sdib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: shia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x38]
+shia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: shib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x28]
+shib a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: swia a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x58]
+swia a0, (a1), 1, 4
+
+# CHECK-ASM-AND-OBJ: swib a0, (a1), 1, 4
+# CHECK-ASM: encoding: [0x0b,0xd5,0x15,0x48]
+swib a0, (a1), 1, 4
+
+
+# CHECK-ASM-AND-OBJ: ldd a2, 0(a1)
+# CHECK-ASM: encoding: [0x0b,0xe6,0x05,0xf8]
+ldd a2, 0(a1)
+
+# CHECK-ASM-AND-OBJ: lwd a2, 0(a1)
+# CHECK-ASM: encoding: [0x0b,0xe6,0x05,0xe0]
+lwd a2, 0(a1)
+
+# CHECK-ASM-AND-OBJ: lwud a2, 0(a1)
+# CHECK-ASM: encoding: [0x0b,0xe6,0x05,0xf0]
+lwud a2, 0(a1)
+
+# CHECK-ASM-AND-OBJ: sdd a2, 0(a1)
+# CHECK-ASM: encoding: [0x0b,0xe6,0x05,0xf8]
+sdd a2, 0(a1)
+
+# CHECK-ASM-AND-OBJ: swd a2, 0(a1)
+# CHECK-ASM: encoding: [0x0b,0xe6,0x05,0xf8]
+swd a2, 0(a1)
+
